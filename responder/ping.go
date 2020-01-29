895a13749fee29d67a8d760b533b290aa620b5cf
VineBalloon <vincentchen2.033@gmail.com>
2020-01-29 23:52:47 +1100

    Updated interface

--- responder/ping.go
+++ responder/ping.go
@@ -7,10 +7,13 @@ import (
 // Ping implements Responder
 type Ping struct{}
 
-// OnMessage
 func (p *Ping) OnMessage(message string) string {
 	if strings.ToLower(message) == "|ping" {
 		return "|pong"
 	}
 	return ""
 }
+
+func (p *Ping) Desc() string { return "Pong!" }
+
+func (p *Ping) Help() string { return "`|ping` returns a `|pong`" }
