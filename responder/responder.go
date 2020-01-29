cf4413aba4440e248568523b51e2a81cfa1bb9d1
VineBalloon <vincentchen2.033@gmail.com>
2020-01-29 23:52:57 +1100

    Updated interface

--- responder/responder.go
+++ responder/responder.go
@@ -4,10 +4,14 @@ package responder
 // Responder is a simple interface for simple commands which just respond to a message
 type Responder interface {
 	OnMessage(message string) string // OnMessage takes a message as a string and responds with some string
+
+	Help() string // Help should provide brief usage instructions
+	Desc() string // Desc should provide a longer description of the command, maybe throw some memes in there
 }
 
 var responders = []Responder{
 	&Ping{},
+	&Help{},
 }
 
 // Notify notifies all Responders and returns a slice of all the responses
